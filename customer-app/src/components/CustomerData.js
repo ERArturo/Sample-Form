8b94dd40f7d55fcd0998d0d4a8e2310261aedc40
Arturo <erarturo@outlook.com>
2020-07-31 13:35:13 -0500

    FEAT: Component CustomerData

---
+++ customer-app/src/components/CustomerData.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CustomerData = ({name, curp, age }) => {
+    return (
+        <div>
+            <div className="customer-data">
+                <h2>Datos del cliente</h2>
+                <div><strong>Nombre: </strong> <i>{name}</i></div>
+                <div><strong>CURP: </strong> <i>{curp}</i></div>
+                <div><strong>Edad: </strong> <i>{age}</i></div>
+            </div>
+        </div>
+    );
+};
+
+CustomerData.propTypes = {
+    name : PropTypes.string.isRequired,
+    curp : PropTypes.string.isRequired,
+    age  : PropTypes.number,
+};
+
+export default CustomerData;
